c512b7c4fa532c35476ce8ef62810b31ac62c459
Shreyash <shreyash@debian-BULLSEYE-live-builder-AMD64>
2023-02-08 01:35:58 +0530

    optional number of students

--- js/registerScript.js
+++ js/registerScript.js
@@ -1,97 +1,40 @@
-$(document).ready(function () {
-    var base_color = "rgb(230,230,230)";
-    var active_color = "rgb(237, 40, 70)";
+const studentDetailsDiv = document.querySelector("#student-details")
+const addBtn = document.getElementById("add-btn")
+const delBtn = document.getElementById("del-btn");
 
+function gen_inp(id){
+    section = document.createElement("section");
+    section.innerHTML =`<p>Student ${id} Details</p>
+        <input type="text" placeholder="Full Name" name="s${id}-name"  />
+        <input type="number" placeholder="Roll Number" name="s${id}-rollno"  />
+        <input type="number" placeholder="Phone Number" name="s${id}-phone"  />
+        <input type="email" placeholder="Email address" name="s${id}-email"  />`
 
+    studentDetailsDiv.appendChild(section);
+}
 
-    var child = 1;
-    var length = $("section").length - 1;
-    $("#prev").addClass("disabled");
-    $("#submit").addClass("disabled");
+var count_students = 1;
 
-    $("section").not("section:nth-of-type(1)").hide();
-    $("section").not("section:nth-of-type(1)").css('transform', 'translateX(100px)');
+function delStudent(){
+    studentDetailsDiv.removeChild(studentDetailsDiv.lastChild)
+    count_students--;
+    setBtnVisibility();
+}
 
-    var svgWidth = length * 200 + 24;
-    $("#svg_wrap").html(
-        '<svg version="1.1" id="svg_form_time" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 ' +
-        svgWidth +
-        ' 24" xml:space="preserve"></svg>'
-    );
+function addAnotherStudent() {
+    count_students++;
+    gen_inp(count_students);
+    setBtnVisibility();
+}
 
-    function makeSVG(tag, attrs) {
-        var el = document.createElementNS("http://www.w3.org/2000/svg", tag);
-        for (var k in attrs) el.setAttribute(k, attrs[k]);
-        return el;
-    }
+function setBtnVisibility(){
+    if(count_students>3)
+        addBtn.classList.add("disabled");
+    else
+        addBtn.classList.remove("disabled");
 
-    for (i = 0; i < length; i++) {
-        var positionX = 12 + i * 200;
-        var rect = makeSVG("rect", { x: positionX, y: 9, width: 200, height: 6 });
-        document.getElementById("svg_form_time").appendChild(rect);
-        // <g><rect x="12" y="9" width="200" height="6"></rect></g>'
-        var circle = makeSVG("circle", {
-            cx: positionX,
-            cy: 12,
-            r: 12,
-            width: positionX,
-            height: 6
-        });
-        document.getElementById("svg_form_time").appendChild(circle);
-    }
-
-    var circle = makeSVG("circle", {
-        cx: positionX + 200,
-        cy: 12,
-        r: 12,
-        width: positionX,
-        height: 6
-    });
-    document.getElementById("svg_form_time").appendChild(circle);
-
-    $('#svg_form_time rect').css('fill', base_color);
-    $('#svg_form_time circle').css('fill', base_color);
-    $("circle:nth-of-type(1)").css("fill", active_color);
-
-
-    $(".button").click(function () {
-        $("#svg_form_time rect").css("fill", active_color);
-        $("#svg_form_time circle").css("fill", active_color);
-        var id = $(this).attr("id");
-        if (id == "next") {
-            $("#prev").removeClass("disabled");
-            if (child >= length) {
-                $(this).addClass("disabled");
-                $('#submit').removeClass("disabled");
-            }
-            if (child <= length) {
-                child++;
-            }
-        } else if (id == "prev") {
-            $("#next").removeClass("disabled");
-            $('#submit').addClass("disabled");
-            if (child <= 2) {
-                $(this).addClass("disabled");
-            }
-            if (child > 1) {
-                child--;
-            }
-        }
-        var circle_child = child + 1;
-        $("#svg_form_time rect:nth-of-type(n + " + child + ")").css(
-            "fill",
-            base_color
-        );
-        $("#svg_form_time circle:nth-of-type(n + " + circle_child + ")").css(
-            "fill",
-            base_color
-        );
-        var currentSection = $("section:nth-of-type(" + child + ")");
-        currentSection.fadeIn();
-        currentSection.css('transform', 'translateX(0)');
-        currentSection.prevAll('section').css('transform', 'translateX(-100px)');
-        currentSection.nextAll('section').css('transform', 'translateX(100px)');
-        $('section').not(currentSection).hide();
-    });
-
-});
+    if(count_students>1)
+        delBtn.classList.remove("disabled");
+    else
+        delBtn.classList.add("disabled");
+}
